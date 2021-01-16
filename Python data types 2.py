3c4a6e9439443bbc9a48f130473949e0c9311817
Kanghyeok Jie <hek1850@naver.com>
2021-01-16 10:23:56 +0900

    2021-01-16 commit!

--- Python data types 2.py
+++ Python data types 2.py
@@ -3,112 +3,112 @@
 
 a = "Life is too short, You need Python"
 
-print (a[0:4])
+print(a[0:4])
 ## Result : Life / [이상:미만:간격] = 첫 글자부터 시작해서 4번 인덱스 전까지 출력.
 
 a = "20010331Rainy"
 
-print (a[:8])
+print(a[:8])
 ## Result : 20010331 / [이상:미만:간격] = 첫 글자부터 시작해서 8번 인덱스전까지 출력.
 
-print (a[8:])
+print(a[8:])
 ## Result : Rainy / [이상:미만:간격] = 8번 인덱스 부터 마지막 인덱스까지 출력.
 
 a = "12345678"
 
-print (a[::1])
+print(a[::1])
 ## Result : 12345678 / [이상:미만:간격] = 첫 인덱스부터 마지막 인덱스까지 한칸 간격으로 출력.
 
-print (a[::2])
+print(a[::2])
 ## Result : 1357 / [이상:미만:간격] = 첫 인덱스부터 마지막 인덱스까지 두칸 간격으로 출력.
 
 # 2.7 문자열 포매팅
 
 a = "I eat %d apples." %3
 
-print (a)
+print(a)
 ## Result : I eat 3 apples. / %d 자리에 뒤에 있는 숫자가 들어감.
 
 a = "I eat %d apples." %3
 b = "I eat " + str(3) + " apples"
 
-print (b)
+print(b)
 ## Result : I eat 3 apples. / b로 쓰는 과정이 복잡하니까 a로 쓰기위해 포매팅 사용.
 
 number = 10
 day = "three"
 a = "I ate %d apples. so I was sick for %s days." % (number, day)
 
-print (a)
+print(a)
 ## Result : I ate 10 apples. so I was sick for three days.
 
 a = "hello ! my name is {name} and i'm {age} years old ." .format(name="kanghyeok",age=23)
 
-print (a)
+print(a)
 ## Result : hello ! my name is kanghyeok and i'm 23 years old .
 
 name = "kanghyeok"
 a = f"나의 이름은 {name}입니다."
 
-print (a)
+print(a)
 ## Result : 나의 이름은 kanghyeok입니다.
 
 # 2.8 정렬과 공백
 a = "%10s" % "hi"
 
-print (a)
+print(a)
 ## Result :         hi / 공백을 둘 수 있다.
 
 # 2.9 소수점 표현
 
 a = "%0.4f" % 3.42134234
 
-print (a)
+print(a)
 ## Result : 3.4213 / %간격.소수점 남기는 자리 수f 를 이용해 소수점을 제한 할 수 있다.
 
 # 2.10 문자열 개수 세기 (count)
 
 a = "hobby"
 
-print (a.count('b'))
+print(a.count('b'))
 ## Result : 2 / a 변수에서 b 가 몇 개 있는지 카운트 하여준다.
 
-print (a.find('b'))
+print(a.find('b'))
 ## Result : 2 / 가장 먼저 나오는 b의 인덱스를 찾아 리턴 하여준다.
 
-print (a.find('x'))
+print(a.find('x'))
 ## Result : -1 / 찾는 값이 존재 하지 않을 경우 -1 을 리턴한다.
 
 # 2.11 문자열 삽입(join)
 
 a = ",".join("abcd")
 
-print (a)
+print(a)
 ## Result : a,b,c,d / abcd에 ,라는 문자열을 삽입 한다.
 
 # 대,소문자를 각각 변환하기, 양쪽 공백 지우기(upper,lower,strip)
 
 a = "hi"
 a = "HI"
-print (a.upper())
-print (a.lower())
+print(a.upper())
+print(a.lower())
 
 ## Result : HI / upper 를 이용하여 소문자를 대문자로 변경.
 ## Result : hi / lower 를 이용하여 대문자를 소문자로 변경.
 
 a = "   HI  "
 
-print (a.strip())
+print(a.strip())
 ## Result : HI / strip 를 이용하여 공백을 삭제 시킴.
 
 # 2.12 문자열 바꾸기,나누기(replace,split)
 
 a = "Life is too short"
 
-print (a.replace("Life", "Your leg"))
+print(a.replace("Life", "Your leg"))
 ## Result : Your leg is too short / replace 함수를 이용하여 Life를 Your leg으로 교체한다.
 
-print (a.split())
+print(a.split())
 ## Result : ['Life', 'is', 'too', 'short'] / 문자열 자료형을 띄어쓰기 기준으로 자르는 함수.
 
 
--- Python data types.py
+++ Python data types.py
@@ -1,12 +1,12 @@
 ## 1. 숫자열 자료형 표현 방법
 a = 1
 
-print (type(a))
+print(type(a))
 ## Result : <class 'int'> int는 정수형 자료형 이란 뜻이다.
 
 a = 1.24
 
-print (type(a))
+print(type(a))
 ## Result : <class 'float'> float은 실수형 자료형 이란 뜻이다.
 
 
@@ -15,22 +15,22 @@ print (type(a))
 a = 3
 b = 4
 
-print (a + b)
+print(a+b)
 ## Result : 7 덧셈을 할 수 있다.
 
-print (a * b)
+print(a*b)
 ## Result : 12 곱셈을 할 수 있다.
 
-print (a / b)
+print(a/b)
 ## Result : 0.75 나눗셈을 할 수 있다.
 
-print (a // b)
+print(a//b)
 ## Result : 0 // 연산자는 나눴을 때 몫을 구할 수 있다.
 
-print (a % b)
+print(a%b)
 ## Result : 3 % 연산자는 나눴을 때 나머지를 구할 수 있다.
 
-print (a ** b)
+print(a**b)
 ## Result : 81 **연산자는 제곱의 값을 구할 수 있다.
 
 
@@ -44,32 +44,32 @@ b = 'Python is fun'
 c = """Life is too short, You need python"""
 d = '''Life is too short, You need python'''
 
-print (a)
-print (type(a))
+print(a)
+print(type(a))
 ## Result : Hello World 
 ## Result : <class 'str'> str은 문자열 자료형 뜻이다.
 
-print (b)
+print(b)
 ## Result : Python is fun
 
-print (c)
+print(c)
 ## Result : Life is too short, You need python
 
-print (d)
+print(d)
 ## Result : Life is too short, You need python
 
 # 2.2 문자열에 따옴표 포함시키기
 
 a = 'Python\'s favorite food is perl'
 
-print (a)
+print(a)
 ## Result : Python's favorite food is perl \ 키를 통해 문자열에 따옴표를 포함 시킬 수 있다.
 
 # 2.3 여러 줄로 이루어진 문자열
 
 a = 'Life is too short\nYou need python'
 
-print (a)
+print(a)
 ## Result : Life is too short
 ##          You need python \n(이스케이프 문자)를 이용하여 여러 줄로 문자열을 만들 수 있다.
 
@@ -77,7 +77,7 @@ a = """Life is too short
 You need
 python"""
 
-print (a)
+print(a)
 ## Result : Life is too short
 ##          You need
 ##          python : 따옴표 3개로 작성하면 이스케이프 문자를 사용하지 않아도 여러 줄의 문자열을 만들 수 있다.
@@ -87,25 +87,25 @@ print (a)
 a = "Python"
 b = " is fun!"
 
-print (a + b)
+print(a+b)
 ## Result : Python is fun!
 
-print (a * 3)
+print(a*3)
 ## Result : PythonPythonPython
 
 # 2.5 인덱싱 (Indexing)
 
 a = "Life is too short, You need python"
 
-print (a[0])
-print (a[1])
-print (a[2])
+print(a[0])
+print(a[1])
+print(a[2])
 ## Result : L
 ## Result : i
 ## Result : f 변수 a에 저장한 문자열에 각 문자마다 번호를 매긴 것을 인덱싱이라 한다.
 
-print (a[-1])
-print (a[-2])
+print(a[-1])
+print(a[-2])
 ## Result : n
 ## Result : o 인덱싱에서 -는 역방향으로 값을 나타낸다.
 
